6c767a947774c84da29b173dd266f29eb0e53382
Sushant Aryal <sushant.aryal90@gmail.com>
2024-01-12 22:30:20 +0545

    Updates

--- assets/About-iT8RAKi6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-DlteLzEa.js";const o={},s={class:"text-white"};function n(r,_){return c(),t("div",s,"About Us Page")}const f=e(o,[["render",n]]);export{f as default};
---
+++ assets/About-uNOfPKzb.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-hAVCOm6Y.js";const o={},s={class:"text-white"};function n(r,_){return c(),t("div",s,"About Us Page")}const f=e(o,[["render",n]]);export{f as default};
